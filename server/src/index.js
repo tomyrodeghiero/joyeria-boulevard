29ee9fc9b885f0880feb88eb77eb94ceee40f4ec
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2023-06-17 19:26:06 -0300

    Update server

--- server/src/index.js
+++ server/src/index.js
@@ -63,7 +63,7 @@ app.get("/api/product/:id", async (req, res) => {
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_APY_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
