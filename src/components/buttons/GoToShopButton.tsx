34933e613ae9c06792fa33580e31585e50548468
Tomás Rodeghiero <tomirodeghiero@Tomass-MacBook-Air.local>
2023-08-14 23:08:25 -0300

    Fix styles

--- src/components/buttons/GoToShopButton.tsx
+++ src/components/buttons/GoToShopButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const GoToShopButton = () => {
   return (
-    <Link href="/shop" className="w-1/4">
+    <Link href="/shop" className="w-1/2 lg:1/4">
       <button className="bg-black w-full hover:bg-white focus:outline-none focus:ring-1 focus:ring-gray-700 focus:ring-opacity-50 transition duration-300 ease-in-out transform hover:shadow-md hover:-translate-y-1 border border-black text-white hover:text-black py-3">
         Ir a la Tienda
       </button>
--- src/components/google-maps/index.tsx
+++ src/components/google-maps/index.tsx
@@ -6,7 +6,7 @@ const GoogleMaps: React.FC = () => {
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3341.4100314466664!2d-64.3539578240228!3d-33.12459088089878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95d201f4e0aa9535%3A0xca3168adbc8cb88f!2sBoulevard!5e0!3m2!1ses-419!2sar!4v1692064413469!5m2!1ses-419!2sar"
       width="100%"
       height="450"
-      style={{ border: 0, marginBottom: "1rem" }}
+      style={{ border: 0, marginBottom: "1rem", marginTop: "0.5rem" }}
       allowFullScreen
       loading="lazy"
       referrerPolicy="no-referrer-when-downgrade"
